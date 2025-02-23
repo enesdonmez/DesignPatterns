f461cc5054e7315b8ba17f3689d77f84a27c8e72
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-02-23 19:25:45 +0300

    Add authentication with AccountController and login view
    - Added AccountController for handling login and logout.
    - Modified AppIdentityDbContext to use custom AppUser class.
    - Updated Program.cs to seed initial users if none exist.
    - Changed launchSettings.json to disable auto browser launch.
    - Created Login.cshtml view for user login interface.
    - Updated _Layout.cshtml to show login/logout links based on authentication status.

--- BaseProject/Program.cs
+++ BaseProject/Program.cs
@@ -21,6 +21,23 @@ public class Program
 
         var app = builder.Build();
 
+        using var scope = app.Services.CreateScope();
+
+        var identityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+        identityDbContext.Database.Migrate();
+
+        if (!userManager.Users.Any())
+        {
+            userManager.CreateAsync(new AppUser { UserName = "user1", Email = "user1@gmail.com" }, "Pass123!").Wait();
+            userManager.CreateAsync(new AppUser { UserName = "user2", Email = "user2@gmail.com" }, "Pass123*").Wait();
+            userManager.CreateAsync(new AppUser { UserName = "user3", Email = "user3@gmail.com" }, "Pass12*").Wait();
+            userManager.CreateAsync(new AppUser { UserName = "user4", Email = "user4@gmail.com" }, "Pass1*-").Wait();
+            userManager.CreateAsync(new AppUser { UserName = "user5", Email = "user5@gmail.com" }, "Password12*").Wait();
+        }
+
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
